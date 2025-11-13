78bb2633bf4a97921c5b3a0f80905c51fcbaf3ba
Steve Higgins <steve.higgins@forbina.co.uk>
2025-11-13 16:44:02 +0000

    Updated variable names for consistency

--- opensearch-setup/initialise.sh
+++ opensearch-setup/initialise.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 OPENSEARCH_URL="${OPENSEARCH_URL:-http://opensearch:9200}"
 ADMIN_USER="${ADMIN_USER:-admin}"
-ADMIN_PASS="${ADMIN_PASS:?set ADMIN_PASS}"
-SECURITY_ROLE_NAME=${SECURITY_ROLE_NAME:-semarchy-data-platform}
+ADMIN_PASSWORD="${ADMIN_PASSWORD:?set ADMIN_PASSWORD}"
 INTERNAL_USER="${INTERNAL_USER:-semarchy-data-platform}"
-INTERNAL_USER_PASS="${INTERNAL_USER_PASS:?set INTERNAL_USER_PASS}"
+INTERNAL_USER_PASSWORD="${INTERNAL_USER_PASSWORD:?set INTERNAL_USER_PASSWORD}"
+SECURITY_ROLE_NAME=${SECURITY_ROLE_NAME:-semarchy-data-platform}
 
 # Wait for Security plugin to report UP (no auth required)
 echo "Waiting for OpenSearch on [${OPENSEARCH_URL}] ..."
@@ -21,7 +21,7 @@ done
 # Create/replace security role 
 echo "Creating security role [${SECURITY_ROLE_NAME}] : "
 curl -sS -X PUT "$OPENSEARCH_URL/_plugins/_security/api/roles/${SECURITY_ROLE_NAME}" \
-  -u "$ADMIN_USER:$ADMIN_PASS" \
+  -u "$ADMIN_USER:$ADMIN_PASSWORD" \
   -H "Content-Type: application/json" \
   -d @/init/role-semarchy-data-platform.json
 echo 
@@ -29,10 +29,10 @@ echo
 # Create/replace an internal user 
 echo "Creating internal user [${INTERNAL_USER}] : "
 curl -sS -X PUT "$OPENSEARCH_URL/_plugins/_security/api/internalusers/${INTERNAL_USER}" \
-  -u "$ADMIN_USER:$ADMIN_PASS" \
+  -u "$ADMIN_USER:$ADMIN_PASSWORD" \
   -H "Content-Type: application/json" \
   -d "{
-        \"password\": \"${INTERNAL_USER_PASS}\",
+        \"password\": \"${INTERNAL_USER_PASSWORD}\",
         \"backend_roles\": [
             \"${SECURITY_ROLE_NAME}\"
           ],
@@ -45,7 +45,7 @@ echo
 # Map/re-map the role to the user
 echo "Mapping security role [${SECURITY_ROLE_NAME}] to user [${INTERNAL_USER}] : "
 curl -sS -X PUT "$OPENSEARCH_URL/_plugins/_security/api/rolesmapping/${SECURITY_ROLE_NAME}" \
-  -u "$ADMIN_USER:$ADMIN_PASS" \
+  -u "$ADMIN_USER:$ADMIN_PASSWORD" \
   -H "Content-Type: application/json" \
   -d "{
         \"users\": [\"${INTERNAL_USER}\"] 
