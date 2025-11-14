3d2206f0fae7d70bf65c50e78ddf1778b7ec2e86
Steve Higgins <steve.higgins@forbina.co.uk>
2025-11-14 11:07:23 +0000

    Small adjustment to shell script

--- list-secrets.sh
+++ list-secrets.sh
@@ -10,4 +10,3 @@ K8S_NAMESPACE=${K8S_NAMESPACE:-semarchy-sdp}
 # List all secrets in the specified namespace and decode their data
 kubectl get secrets -n "$K8S_NAMESPACE" -o json \
   | jq -r '.items[] | "\(.metadata.name):", (.data // {} | to_entries[] | "  \(.key)=\(.value | @base64d)")'
-
