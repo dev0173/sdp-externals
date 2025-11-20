42e2951e0d9fd323f2ebbdbf9a4e78db9c984bd2
Steve Higgins <steve.higgins@forbina.co.uk>
2025-11-20 15:06:59 +0000

    Added shell script to clear down the Docker environment

---
+++ docker-reset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Reset the docker environment by stopping and deleteing the containers, and deleting the volumes
+
+set -euo pipefail
+
+docker compose down
+
+docker volume rm \
+  sdp-externals_postgres-data \
+  sdp-externals_kafka-data \
+  sdp-externals_mailpit-data \
+  sdp-externals_opensearch-data \
+  sdp-externals_kafka-data-secrets \
+  sdp-externals_kafka-init \
+  sdp-externals_kafka-shared-config
+
